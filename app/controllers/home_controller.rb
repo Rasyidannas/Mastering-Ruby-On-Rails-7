880e82186fafb5560d044eda7480a0b66064616e
rasyidannas <rasshit.dsgn@gmail.com>
2024-10-16 20:34:34 +0700

    2/3. Accessing Request QueryString Parameters.mp4

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,4 +9,24 @@ class HomeController < ApplicationController
 
     render html: message.html_safe
   end
+
+  def getquerystringvalues
+    message=""
+    if params[:message]
+      message+="<h1>Message is :" + params[:message] + "</h1>"
+    else
+      message+="<h1>You havent supplied any value for message...</h1>"
+    end
+
+    if params[:country]
+      message+="<h1>Message is :" + params[:country] + "</h1>"
+    else
+      message+="<h1>You havent supplied any value for message...</h1>"
+    end
+
+    message += "<h1>Controller name is:" + params[:controller] + "</h1>"
+    message += "<h1>Action name is:" + params[:action] + "</h1>"
+
+    render html: message.html_safe
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,5 @@ Rails.application.routes.draw do
   # root "posts#index"
 
   get "home/index" => "home#index"
+  get "home/getquerystringvalues" => "home#getquerystringvalues"
 end
