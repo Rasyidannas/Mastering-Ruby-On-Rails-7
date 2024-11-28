8015199f6306082215416109dd508503b78ba7c8
rasyidannas <rasshit.dsgn@gmail.com>
2024-11-28 07:57:42 +0700

    4/2. Working with Debug Gem.mp4

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -52,7 +52,7 @@ class HomeController < ApplicationController
 
   def loopthrougharrays
     @Products = []
-
+    debugger
     @Products.push({ 'productID': 1, 'productName': "AMD Ryzen 3990", 'quantity': 100, 'unitsInStock': 50, 'disContinued': "False", 'cost': 3000 })
 
     @Products.push({ 'productID': 2, 'productName': "Intel Core i9-10900K", 'quantity': 80, 'unitsInStock': 40, 'disContinued': "False", 'cost': 500 })
