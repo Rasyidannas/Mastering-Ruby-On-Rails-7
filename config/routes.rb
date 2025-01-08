3b2d72243a329b3067d4a920e451e664a1156394
rasyidannas <rasshit.dsgn@gmail.com>
2025-01-08 08:09:34 +0700

    8/5. Editing Deatils of an Employee.mp4

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,6 @@ Rails.application.routes.draw do
   # root "posts#index"
   
   get "employees" => "employees#index", as: "employees"
+  get "employees/:id" => "employees#edit", as: "edit_employee"
   get "employees/:id" => "employees#show", as: "employee"
 end
