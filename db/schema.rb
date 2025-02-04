94cf3fe13c2e7d1974c3f9fdf1a4bfbec40ab959
rasyidannas <rasshit.dsgn@gmail.com>
2025-02-04 12:41:18 +0700

    11/1. Setting Up Validations Demo Application.mp4

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_01_20_034146) do
+ActiveRecord::Schema[7.2].define(version: 2025_02_04_053635) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -81,6 +81,21 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_20_034146) do
     t.bigint "department_id", null: false
     t.index ["country_id"], name: "index_employees_on_country_id"
     t.index ["department_id"], name: "index_employees_on_department_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "password_digest"
+    t.date "birthdate"
+    t.string "email"
+    t.string "phone"
+    t.integer "postalcode"
+    t.string "websiteurl"
+    t.boolean "termsandconditions"
+    t.string "role"
+    t.string "notes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
