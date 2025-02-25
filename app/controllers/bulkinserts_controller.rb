7cdc8e7d8a7276057afba1b3ca2cadbe93f8467b
rasyidannas <rasshit.dsgn@gmail.com>
2025-02-25 15:45:25 +0700

    15/3. Inserting Bulk Records.mp4

--- app/controllers/bulkinserts_controller.rb
+++ app/controllers/bulkinserts_controller.rb
@@ -4,5 +4,15 @@ class BulkinsertsController < ApplicationController
   end
 
   def create
+    employees_params.each do |employee_params|
+      employee=Employee.create(employee_params)
+    end
+    redirect_to new_bulkinsert_path
+  end
+
+  def employees_params
+    params.require(:employees).map do |employee_params|
+      employee_params.permit(:firstname, :lastname, :department_id, :country_id, :email, :phone)
+    end
   end
 end
