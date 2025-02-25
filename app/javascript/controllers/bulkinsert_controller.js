d1de7e6c5553ed3ec4ecd08967e1d127d9798421
rasyidannas <rasshit.dsgn@gmail.com>
2025-02-25 11:17:36 +0700

    15/2. Rendering Partial View From Main View.mp4

--- app/javascript/controllers/bulkinsert_controller.js
+++ app/javascript/controllers/bulkinsert_controller.js
@@ -5,6 +5,13 @@ export default class extends Controller {
   connect() {
   }
 
+  static targets = ["container", "template"]
+
+  addEmployee() {
+    const clone = document.importNode(this.templateTarget.content, true);
+    this.containerTarget.appendChild(clone);
+  }
+
   removeEmployee(event) {
     event.target.closest("div").remove();
   }
