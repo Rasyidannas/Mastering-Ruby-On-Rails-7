91452bc53695d3dad8762e2ab9d57cbff5a97f11
rasyidannas <rasshit.dsgn@gmail.com>
2025-03-03 11:07:35 +0700

    17/2. Filling Dropdown List with Categories.mp4

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+//= require jquery
+//= require rails-ujs
+//= require toastr
+
 import "@hotwired/turbo-rails"
 import "controllers"
