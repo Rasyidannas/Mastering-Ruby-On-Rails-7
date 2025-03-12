a3bcda2018fa16d8b4a7c1ffa004f3ddf3eca5b3
rasyidannas <rasshit.dsgn@gmail.com>
2025-03-12 16:21:47 +0700

    18/3. implementing Dynamic Page Sizing.mp4

--- app/controllers/employeespaginations_controller.rb
+++ app/controllers/employeespaginations_controller.rb
@@ -3,6 +3,8 @@ class EmployeespaginationsController < ApplicationController
     #Pagination
     # @employees = Employee.all
     # @employees = Employee.page(params[:page])
-  @employees = Employee.page(params[:page]).per(5)
+    # @employees = Employee.page(params[:page]).per(5)
+
+    @employees = Employee.page(params[:page]).per(params[:per_page])
   end
 end
