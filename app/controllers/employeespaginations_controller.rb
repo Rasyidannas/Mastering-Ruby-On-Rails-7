e03c870280c78b2f2847f16c8f8de88184f395e0
rasyidannas <rasshit.dsgn@gmail.com>
2025-03-12 20:39:57 +0700

    18/4. Implementing Search Functionality.mp4

--- app/controllers/employeespaginations_controller.rb
+++ app/controllers/employeespaginations_controller.rb
@@ -5,6 +5,8 @@ class EmployeespaginationsController < ApplicationController
     # @employees = Employee.page(params[:page])
     # @employees = Employee.page(params[:page]).per(5)
 
-    @employees = Employee.page(params[:page]).per(params[:per_page])
+    # @employees = Employee.page(params[:page]).per(params[:per_page])
+
+    @employees = Employee.search(params[:search]).page(params[:page]).per(params[:per_page] || 5)
   end
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -4,4 +4,16 @@ class Employee < ApplicationRecord
   belongs_to :country
   belongs_to :department
   has_rich_text :notes
+
+  # It's a way to name a custom database query, composed of ActiveRecord methods.
+  scope :search, ->(query) { 
+  if query.present?
+    left_joins(:department)
+      .where("CAST(employees.id AS TEXT) LIKE ? OR firstname LIKE ? OR lastname LIKE ? OR departments.name LIKE ?", 
+             "%#{query}%", "%#{query}%", "%#{query}%", "%#{query}%")
+  else
+    all
+  end
+}
+
 end
